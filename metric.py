fix overflow issue with crossentropy in numpy

metric.py
@@ -1,6 +1,5 @@
 import numpy as np
 from scipy.sparse import issparse
-from scipy.special import expit
 
 
 def ndcg_binary_at_k_batch(x_pred, x_true, k=100):
@@ -42,15 +41,8 @@ def recall_at_k_batch(x_pred, x_true, k=100):
     return recall
 
 
-def binary_crossentropy_batch(x_pred, x_true, from_logits=True):
+def binary_crossentropy_from_logits(x_pred, x_true):
     x_pred = x_pred.toarray() if issparse(x_pred) else np.array(x_pred)
     x_true = x_true.toarray() if issparse(x_true) else np.array(x_true)
-    if from_logits:
-        x_pred = expit(x_pred)
 
-    print(x_pred.shape, x_true.shape)
-    pos_term = x_true * np.log2(x_pred)
-    neg_term = (1 - x_true) * np.log2(1 - x_pred)
-    bce = -np.mean(pos_term + neg_term, axis=1)
-
-    return bce
+    return np.max(x_pred, 0) - x_pred * x_true + np.log2(1 + 2**(-np.abs(x_pred)))

models/tf.py
@@ -9,7 +9,7 @@ import tensorflow as tf
 from scipy.sparse import issparse
 from tensorflow.contrib.layers import apply_regularization, l2_regularizer
 
-from metric import ndcg_binary_at_k_batch, recall_at_k_batch, binary_crossentropy_batch
+from metric import ndcg_binary_at_k_batch, recall_at_k_batch, binary_crossentropy_from_logits
 
 
 @gin.configurable
@@ -165,12 +165,12 @@ def evaluate(model, sess, x_val, y_val, batch_size=100, metric_logger=None):
         y_pred[x.nonzero()] = -np.inf
         ndcg_list.append(ndcg_binary_at_k_batch(y_pred, y, k=100))
         r100_list.append(recall_at_k_batch(y_pred, y, k=100))
-        bce_list.append(binary_crossentropy_batch(y_pred, y))
+        bce_list.append(binary_crossentropy_from_logits(y_pred, y))
         loss_list.append(ae_loss)
 
     val_ndcg = np.concatenate(ndcg_list).mean()  # mean over n_val
     val_r100 = np.concatenate(r100_list).mean()
-    val_bce = np.concatenate(bce_list).mean()
+    val_bce = np.concatenate(bce_list).nanmean()
     val_loss = np.mean(loss_list)  # mean over batches
     metrics = {'val_ndcg': val_ndcg, 'val_r100': val_r100, 'val_bce': val_bce,
                'val_loss': val_loss}