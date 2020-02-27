adf633d3aacc3d16e456f2faa10e8f94dec724a2
Jan Van Balen <jan.vanbalen@uantwerpen.be>
2020-02-27 16:48:20 +0100

    big bugfix in preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -1,10 +1,22 @@
 
+import gin
+from scipy.sparse import issparse
 from sklearn.preprocessing import Normalizer
 from sklearn.feature_extraction.text import TfidfTransformer
 
 
+@gin.configurable
 def preprocess(x_train, y_train, x_val, y_val, tfidf=False, norm=None,
                process_labels=False):
+
+    if issparse(x_train):
+        x_train = x_train.tocsr()
+    if issparse(y_train):
+        y_train = y_train.tocsr()
+    if issparse(x_val):
+        x_val = x_val.tocsr()
+    if issparse(y_val):
+        y_val = y_val.tocsr()
 
     if tfidf:
         trans = TfidfTransformer(norm=norm)
@@ -14,9 +26,9 @@ def preprocess(x_train, y_train, x_val, y_val, tfidf=False, norm=None,
         return x_train, y_train, x_val, y_val
 
     x_train = trans.fit_transform(x_train)
-    x_val - trans.transform(x_val)
+    x_val = trans.transform(x_val)
     if process_labels:
-        y_train - trans.transform(y_train)
-        y_val - trans.transform(y_val)
+        y_train = trans.transform(y_train)
+        y_val = trans.transform(y_val)
 
     return x_train, y_train, x_val, y_val
