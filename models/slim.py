6f016b423b605ede6577f8e32792d41de9a7b8d5
Jan Van Balen <jan.vanbalen@uantwerpen.be>
2020-03-27 16:37:52 +0100

    a lot of different semi-tested SLIM variants

--- models/slim.py
+++ models/slim.py
@@ -4,16 +4,17 @@ from warnings import warn
 
 import gin
 import numpy as np
-from scipy.sparse import issparse, csr_matrix, lil_matrix, coo_matrix, eye
+from scipy.sparse import csr_matrix, coo_matrix, issparse, eye
 
 from models.base import BaseRecommender
-from util import prune_global, load_weights, get_pruning_threshold
+from util import prune_global, load_weights, get_pruning_threshold, sparse_info
 
 
 @gin.configurable
 class LinearRecommender(BaseRecommender):
 
-    def __init__(self, log_dir, reg=500, density=1.0, batch_size=100, save_weights=True):
+    def __init__(self, log_dir, reg=500, density=1.0, batch_size=100, save_weights=True,
+                 incremental=False, head_tail=False):
         """Linear recommender based on Harald Steck's closed form variant [1]
         of Sparse Linear Methods (SLIM) [2].
 
@@ -27,6 +28,8 @@ class LinearRecommender(BaseRecommender):
         self.reg = reg
         self.density = density
         self.save_weights = save_weights
+        self.incremental = incremental
+        self.head_tail = head_tail
         self.weights = None
         super().__init__(log_dir, batch_size=batch_size)
 
@@ -36,10 +39,22 @@ class LinearRecommender(BaseRecommender):
             warn("SLIM is unsupervised, y_train will be ignored")
 
         t1 = time.perf_counter()
-        gramm = gramm_matrix(x_train).toarray()
-        self.weights = closed_form_slim(gramm, l2_reg=self.reg)
-        if self.density < 1.0:
-            self.weights = prune_global(self.weights, self.density)
+        if self.incremental and self.head_tail:
+            self.weights = block_slim(x_train, l2_reg=self.reg, target_density=self.density)
+            if self.density < 1.0:
+                self.weights = prune_global(self.weights, self.density)
+        elif self.incremental:
+            self.weights = incremental_slim(x_train, batch_size=self.batch_size, target_density=self.density)
+            # self.weights = naive_incremental_slim(x_train, batch_size=self.batch_size, target_density=self.density)
+        elif self.head_tail:
+            self.weights = head_tail_slim(x_train, l2_reg=self.reg, target_density=self.density)
+            if self.density < 1.0:
+                self.weights = prune_global(self.weights, self.density)
+        else:
+            gramm = (x_train.T @ x_train).toarray()
+            self.weights = closed_form_slim(gramm, l2_reg=self.reg)
+            if self.density < 1.0:
+                self.weights = prune_global(self.weights, self.density)
         dt = time.perf_counter() - t1
         if self.logger is not None:
             self.logger.log_config(gin.operative_config_str())
@@ -92,7 +107,7 @@ class WoodburyRecommender(LinearRecommender):
                                          target_density=self.density)
 
         dt = time.perf_counter() - t0
-        if self.logger is not None:
+        if self.logger is not None: 
             self.logger.log_config(gin.operative_config_str())
             if self.save_weights:
                 self.logger.save_weights(self.weights)
@@ -134,6 +149,8 @@ class LinearRecommenderFromFile(BaseRecommender):
 
 def closed_form_slim(gramm, l2_reg=500):
 
+    print(f'gramm.max() = {gramm.max()}')
+    raise ValueError()
     if issparse(gramm):
         gramm = gramm.toarray()
     print(f'  computing slim weights of shape {gramm.shape}')
@@ -146,78 +163,137 @@ def closed_form_slim(gramm, l2_reg=500):
     return weights
 
 
-def block_closed_form_slim(x, l2_reg=500, batch_size=100, density=1.0):
+def distilled_slim(gramm, fn_name='closed_form', n_distillations=1, l2_reg=500):
 
-    n_users, n_items = x.shape
-    weights = coo_matrix((n_items, n_items))
-    for x_batch in gen_batches(x, batch_size=batch_size):
-        gramm = gramm_matrix(x)
-        gramm.eliminate_zeros()
-        nonzero_rows = np.unique(gramm.tocoo().row)
-
-        sub_gramm = gramm[nonzero_rows][:, nonzero_rows]
-        sub_weights = csr_matrix(closed_form_slim(sub_gramm, l2_reg=l2_reg))
-
-        rows = lil_matrix((sub_weights.shape[0], gramm.shape[1]))
-        rows[:, nonzero_rows] = lil_matrix(sub_weights)
-        batch_weights = lil_matrix(gramm.shape)
-        batch_weights[nonzero_rows] = rows
-        if density < 1.0:
-            batch_weights = prune_global(batch_weights, density)
-        weights += batch_weights.tocoo()
-    n_batches = np.ceil(n_users / batch_size)
-    weights = weights.tocsr() / n_batches
+    slim_fns = {'closed_form': closed_form_slim}
+    
+    slim_fn = slim_fns[fn_name]
+    weights = slim_fn(gramm, l2_reg=l2_reg)
+    for i in range(n_distillations):
+        print('DISTILL ITERATION {i}...')
+        gramm = weights @ gramm @ weights
+        weights = slim_fn(weights, l2_reg=l2_reg)
 
     return weights
 
 
-def woodblocks(x, l2_reg=500, batch_size=100, density=1.0, eps=0.001, sparse_gramm=False):
-    """Batched computation of the inverse gramm matrix
+def head_tail_slim(x, l2_reg=500, target_density=0.01, gramm_mass_thr=0.1):
 
-    steps:
-        G is the gramm matrix of the updates
-        U, V index the submatrix C of G such that C = V @ G @ U
+    n_users, n_items = x.shape
+    t = time.perf_counter()
 
-        C = V @ G @ U
-        B = inv(C) + V @ W @ U
-        W = W - W @ U @ inv(B) @ V @ W
+    print('- computing gramm matrix...')
+    gramm = x.T @ x
+    B = csr_matrix(gramm.shape)
+    print(f'  elapsed: {time.perf_counter() - t}')
+    t = time.perf_counter()
+
+    print('- computing head and tail...')
+    head, tail = get_head_tail(gramm, gramm_mass_thr)
+    print(f'  elapsed: {time.perf_counter() - t}')
+    t = time.perf_counter()
+
+    G_head = gramm[head][:, head].toarray()
+    print(f'- computing head weights - shape {G_head.shape}...')
+    diag_indices = np.diag_indices(len(head))
+    G_head[diag_indices] += l2_reg
+    P_head = np.linalg.inv(G_head)
+    B_head = P_head / (-np.diag(P_head))
+    B_head[diag_indices] = 0.0
+    block_density = min(1.0, target_density * np.prod(B.shape) / np.prod(B_head.shape))
+    B_head = prune_global(B_head, target_density=block_density).tocoo()
+    B += csr_matrix((B_head.data, (head[B_head.row], head[B_head.col])), shape=B.shape)
+    print(f'  elapsed: {time.perf_counter() - t}')
+    t = time.perf_counter()
+
+    G_head_tail = gramm[head][:, tail].toarray()
+    print(f'- computing head x tail weights - shape {G_head_tail.shape}...')
+    B_tail = P_head @ G_head_tail
+    block_density = min(1.0, target_density * np.prod(B.shape) / np.prod(B_tail.shape))
+    B_tail = prune_global(B_tail, target_density=block_density).tocoo()
+    dB = csr_matrix((B_tail.data, (head[B_tail.row], tail[B_tail.col])), shape=B.shape)
+    B += dB + dB.T
+    print(f'  elapsed: {time.perf_counter() - t}')
+
+    return B
+
+
+def get_head_tail(gramm, thr=0.1):
+
+    col_sum = np.ravel(gramm.sum(axis=0))
+    items = np.argsort(-col_sum)
+
+    tot_gramm = gramm.sum()
+    cum_gramm = np.cumsum([gramm[item, item] + 2 * gramm[items[i:], item].sum() for i, item in enumerate(items)])
+    i = np.nonzero(cum_gramm > (1 - thr) * tot_gramm)[0][0]
+
+    return items[:i], items[i:]
+
+
+def block_slim(x, l2_reg=500, target_density=0.01, block_size=3000, mass_thr=0.00):
+    """Compute SLIM weights using Steck's closed form solution,
+    but in (large) batches.
     """
-    n_users, n_items = x.shape
-    n_batches = np.ceil(n_users / batch_size)
-    inv_gramm = eye(n_items).tocsr() if sparse_gramm else np.eye(n_items)
-    inv_gramm /= l2_reg
-    for x_batch in gen_batches(x, batch_size=batch_size):
+    t = time.perf_counter()
+    print('- computing gramm matrix...')
+    gramm = x.T @ x
+    gramm_edges = gramm.copy()
+    gramm_edges.data = np.ones_like(gramm_edges.data, dtype=int)
+    row_degrees = np.ravel(gramm_edges.sum(axis=0))
+    tail = np.argsort(-row_degrees)
+
+    B = csr_matrix(gramm.shape)
+    thr = 0.0
+    gramm_mass = gramm.sum()
+    remaining_mass = gramm_mass
+    while remaining_mass / gramm_mass > mass_thr:
+        head, tail = tail[:block_size], tail[block_size:]
         
-        inds = np.unique(x_batch.tocoo().col)
-        rank = len(inds)
+        G_head = gramm[head][:, head].toarray()
+        print(f'  elapsed: {time.perf_counter() - t}...')
+        t = time.perf_counter()
+        print(f'- computing weights block of shape {G_head.shape}...')
+        diag_indices = np.diag_indices(len(head))
+        G_head[diag_indices] += l2_reg
+        P_head = np.linalg.inv(G_head)
+        B_head = P_head / (-np.diag(P_head))
+        B_head[diag_indices] = 0.0
+        print(f'  elapsed: {time.perf_counter() - t}...')
+        t = time.perf_counter()
+        print('- updating weights...')
+        B_head[np.abs(B_head) < thr] = 0
+        B_head = coo_matrix(B_head)
+        dB = csr_matrix((B_head.data, (head[B_head.row], head[B_head.col])), shape=B.shape)
+        B += dB
+        print(f'  elapsed: {time.perf_counter() - t}...')
+        t = time.perf_counter()
+        print('- pruning weights')
 
-        gramm_batch_sub = gramm_matrix(x_batch[:, inds])
-        gramm_batch_sub.eliminate_zeros()
-        if eps > 0.0:
-            diag_indices = np.diag_indices(rank)
-            gramm_batch_sub[diag_indices] += eps * l2_reg / n_batches
+        thr = get_pruning_threshold(B, target_density=target_density)
+        B.data[np.abs(B.data) < thr] = 0.0
+        B.eliminate_zeros()
 
-        inv_gramm_sub = inv_gramm[inds][:, inds]
-        print(f'  inverting dense array of rank {rank}...')
-        if sparse_gramm:
-            inv_gramm_sub = inv_gramm_sub.toarray()
-        inv_rel_update_sub = np.linalg.inv(gramm_batch_sub.toarray()) + inv_gramm_sub
-        print(f'  inverting another dense array of rank {rank}...')
-        rel_update_sub = np.linalg.inv(inv_rel_update_sub)
-        print(f'  multiplying inverse gramm with relative update...')  # expensive
-        if sparse_gramm:
-            rel_update_sub = csr_matrix(rel_update_sub)
-        update = inv_gramm[:, inds] @ rel_update_sub @ inv_gramm[inds]
-        print(f'  updating the inverse gramm...')
-        inv_gramm -= update
-            
-    diag_indices = np.diag_indices(n_items)
-    weights = - inv_gramm / inv_gramm.diagonal()
-    if sparse_gramm:
-        weights = csr_matrix(weights)
-    weights[diag_indices] = 0.0
+        G_tail = gramm[tail][:, tail]
+        remaining_mass = G_tail.sum()
+        print(f'  remaining gramm mass = {remaining_mass / gramm_mass}...')
 
-    return weights
+        if len(tail) == 0:
+            continue
+
+        G_head_tail = gramm[head][:, tail].toarray()
+        print(f'  elapsed: {time.perf_counter() - t}...')
+        t = time.perf_counter()
+        print(f'- computing weights block of shape {G_head_tail.shape}...')
+        B_head_tail = P_head @ G_head_tail
+        print(f'  elapsed: {time.perf_counter() - t}...')
+        t = time.perf_counter()
+        print('- updating weights...')
+        B_head_tail[np.abs(B_head_tail) < thr] = 0
+        B_head_tail = coo_matrix(B_head_tail)
+        dB = csr_matrix((B_head_tail.data, (head[B_head_tail.row], tail[B_head_tail.col])), shape=B.shape)
+        B = B + dB + dB.T
+
+    return B
 
 
 def woodbury_slim(x, l2_reg=500, batch_size=100, drop_inactive_items=False, target_density=1.0):
@@ -259,7 +335,8 @@ def woodbury_slim(x, l2_reg=500, batch_size=100, drop_inactive_items=False, targ
     return weights
 
 
-def woodbury_slim_approx(x, l2_reg=500, batch_size=100, target_density=1.0, strict_sparsity=True):
+def woodbury_slim_approx(x, l2_reg=500, batch_size=100, target_density=1.0,
+                         extra_reg=500, strict_sparsity=True):
     """Closed-form SLIM with incremental computation of the inverse gramm matrix,
     and a useful approximation.
 
@@ -278,8 +355,9 @@ def woodbury_slim_approx(x, l2_reg=500, batch_size=100, target_density=1.0, stri
         for most operations this does not affect the solution, however
         in our last step, it does, making this method an approximation
     - optionally prune each update to conserve memory
+    - optionally add extra regularization to stabilize training
     """
-    n_items = x.shape[1]
+    n_users, n_items = x.shape
     P = eye(n_items).tocsr() / l2_reg
     for x_batch in gen_batches(x, batch_size=batch_size):
         print('  subsetting...')
@@ -290,7 +368,7 @@ def woodbury_slim_approx(x, l2_reg=500, batch_size=100, target_density=1.0, stri
         XSPS = (x_batch @ SPS).toarray()
         XSPSX = XSPS @ x_batch.T
         print('  inverse...')
-        B = np.linalg.inv(np.eye(x_batch.shape[0]) + XSPSX)
+        B = np.linalg.inv(np.eye(x_batch.shape[0]) * (1.0 + extra_reg) + XSPSX)
         print('  computing update...')
         dSPS = - XSPS.T @ B @ XSPS
         SPS_new = SPS.toarray() + dSPS
@@ -315,6 +393,55 @@ def woodbury_slim_approx(x, l2_reg=500, batch_size=100, target_density=1.0, stri
     return weights
 
 
+def incremental_slim(x, batch_reg=5, batch_size=100, target_density=1.0):
+    """Closed-form SLIM with incremental computation of the inverse gramm matrix,
+    and a useful approximation.
+
+    Compute a SLIM matrix for each batch (over 'active items' only)
+    and average after pruning
+    """
+    n_users, n_items = x.shape
+    n_batches = int(np.ceil(n_users / batch_size))
+    P = eye(n_items).tocsr() / batch_reg * n_batches
+    for x_batch in gen_batches(x, batch_size=batch_size):
+        print('  submatrix...')
+        X, cols = drop_empty_cols(x_batch)
+        print('  computing update...')
+        B = np.linalg.inv(np.eye(x_batch.shape[0]) * batch_reg + X @ X.T)
+        XBX = X.T @ B @ X
+        P_sub = - XBX / batch_reg
+        print('  updating...')
+        P = add_submatrix(P, P_sub, where=(cols, cols), target_density=target_density)
+
+    diag_indices = np.diag_indices(n_items)
+    inv_diag = 1./P.diagonal()
+    inv_diag[P.diagonal() == 0] = 1.0
+    weights = - P.multiply(inv_diag).tocsr()
+    weights[diag_indices] = 0.0
+
+    return weights
+
+
+def naive_incremental_slim(x, batch_reg=5, batch_size=100, target_density=1.0):
+    """Closed-form SLIM with incremental computation of the inverse gramm matrix,
+    and a useful approximation.
+
+    Compute a SLIM matrix for each batch (over 'active items' only)
+    and average after pruning
+    """
+    n_users, n_items = x.shape
+    B = csr_matrix((n_items, n_items))
+    for x_batch in gen_batches(x, batch_size=batch_size):
+        print('  submatrix...')
+        XS, cols = drop_empty_cols(x_batch)
+        print('  computing slim B for batch...')
+        B_sub = closed_form_slim(XS.T @ XS, l2_reg=batch_reg)
+        print('  updating...')
+        B = add_submatrix(B, B_sub, where=(cols, cols), target_density=target_density)
+
+    return B
+
+
 def woodbury_ensemble(x, n_models=30, l2_reg=500, batch_size=100, target_density=1.0, strict_sparsity=True):
 
     weights = 0
@@ -331,6 +458,37 @@ def woodbury_ensemble(x, n_models=30, l2_reg=500, batch_size=100, target_density
     return weights
 
 
+def drop_empty_cols(X):
+
+    active_cols = np.unique(X.tocoo().col)
+    X_sub = X.tocsc()[:, active_cols]
+
+    return X_sub, active_cols
+
+
+def add_submatrix(A, sub, where, target_density=1.0, verbose=False):
+    """Add submatrix `sub` to `A` at indices `where = (rows, cols)`.
+    Optionally ensure the result is sparse with density `target_density`
+    """
+    rows, cols = where
+    if target_density < 1.0:
+        thr = get_pruning_threshold(A.tocsr(), target_density=target_density)
+        sub_before = A[rows][:, cols]
+        sub_after = sub_before.toarray() + sub
+        sub_after[np.abs(sub_after) < thr] = 0.0
+        sub = (csr_matrix(sub_after) - sub_before).tocoo()
+    dA = csr_matrix((sub.data, (rows[sub.row], cols[sub.col])), shape=A.shape)
+    A += dA
+    if target_density < 1.0:
+        A.data[np.abs(A.data) < thr] = 0.0
+        A.eliminate_zeros()
+    if verbose:
+        print(f'  update size = ({len(rows)}, {len(cols)})')
+        print(f'  max(abs(A)) = {np.max(np.abs(A))}')
+
+    return A
+
+
 def gen_batches(x, batch_size=100):
 
     n_examples = x.shape[0]
@@ -340,7 +498,3 @@ def gen_batches(x, batch_size=100):
         end = min(start + batch_size, n_examples)
         yield x[start:end]
 
-
-def gramm_matrix(x):
-
-    return x.T.dot(x)
