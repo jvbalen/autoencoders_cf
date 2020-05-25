fa56dd7c729da470482d29b554661c5f1a980093
Jan Van Balen <jan.vanbalen@uantwerpen.be>
2020-05-25 18:23:59 +0200

    param sweep script

---
+++ config/sweep_chol_01.sh
@@ -0,0 +1,10 @@
+
+python run.py --data ~/data/msd/ --logdir ~/experiments/autoencoders/msd/ --config config/chol_100_100.gin
+python run.py --data ~/data/msd/ --logdir ~/experiments/autoencoders/msd/ --config config/chol_100_200.gin
+python run.py --data ~/data/msd/ --logdir ~/experiments/autoencoders/msd/ --config config/chol_100_400.gin
+python run.py --data ~/data/msd/ --logdir ~/experiments/autoencoders/msd/ --config config/chol_140_100.gin
+python run.py --data ~/data/msd/ --logdir ~/experiments/autoencoders/msd/ --config config/chol_140_200.gin
+python run.py --data ~/data/msd/ --logdir ~/experiments/autoencoders/msd/ --config config/chol_140_400.gin
+python run.py --data ~/data/msd/ --logdir ~/experiments/autoencoders/msd/ --config config/chol_200_100.gin
+python run.py --data ~/data/msd/ --logdir ~/experiments/autoencoders/msd/ --config config/chol_200_200.gin
+python run.py --data ~/data/msd/ --logdir ~/experiments/autoencoders/msd/ --config config/chol_200_400.gin
