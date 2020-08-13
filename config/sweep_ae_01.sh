5a5d16e5ae33fac71740e91054b525fe8242dbfc
Jan Van Balen <jan.vanbalen@uantwerpen.be>
2020-08-13 15:09:08 +0200

    clean up config files, pt. 2

--- config/sweep_ae_01.sh
+++
@@ -1,4 +0,0 @@
-
-python run.py --data ~/data/msd/ --logdir ~/experiments/autoencoders/msd/ --config config/ae_200.gin
-python run.py --data ~/data/msd/ --logdir ~/experiments/autoencoders/msd/ --config config/ae_400.gin
-python run.py --data ~/data/msd/ --logdir ~/experiments/autoencoders/msd/ --config config/ae_600.gin
