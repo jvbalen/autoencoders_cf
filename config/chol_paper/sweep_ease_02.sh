c96998b2ae57e679201e1738d703a0a7d08df547
Jan Van Balen <jan.vanbalen@uantwerpen.be>
2020-08-13 15:01:02 +0200

    clean up config files, pt. 1

---
+++ config/chol_paper/sweep_ease_02.sh
@@ -0,0 +1,17 @@
+
+python run.py --data ~/data/msd/ --logdir ~/experiments/autoencoders/msd/ --config config/ease_100_004.gin
+python run.py --data ~/data/msd/ --logdir ~/experiments/autoencoders/msd/ --config config/ease_100_002.gin
+python run.py --data ~/data/msd/ --logdir ~/experiments/autoencoders/msd/ --config config/ease_100_001.gin
+python run.py --data ~/data/msd/ --logdir ~/experiments/autoencoders/msd/ --config config/ease_140_004.gin
+python run.py --data ~/data/msd/ --logdir ~/experiments/autoencoders/msd/ --config config/ease_140_002.gin
+python run.py --data ~/data/msd/ --logdir ~/experiments/autoencoders/msd/ --config config/ease_140_001.gin
+python run.py --data ~/data/msd/ --logdir ~/experiments/autoencoders/msd/ --config config/ease_200_004.gin
+python run.py --data ~/data/msd/ --logdir ~/experiments/autoencoders/msd/ --config config/ease_200_002.gin
+python run.py --data ~/data/msd/ --logdir ~/experiments/autoencoders/msd/ --config config/ease_200_001.gin
+python run.py --data ~/data/msd/ --logdir ~/experiments/autoencoders/msd/ --config config/ease_280_004.gin
+python run.py --data ~/data/msd/ --logdir ~/experiments/autoencoders/msd/ --config config/ease_280_002.gin
+python run.py --data ~/data/msd/ --logdir ~/experiments/autoencoders/msd/ --config config/ease_280_001.gin
+
+python run.py --data ~/data/msd/ --logdir ~/experiments/autoencoders/msd/ --config config/chol_100_280.gin
+python run.py --data ~/data/msd/ --logdir ~/experiments/autoencoders/msd/ --config config/chol_140_280.gin
+python run.py --data ~/data/msd/ --logdir ~/experiments/autoencoders/msd/ --config config/chol_200_280.gin
